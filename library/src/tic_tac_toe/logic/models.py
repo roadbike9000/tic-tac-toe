b4775e936b78da743f65895c103ee9189ae641fd
Jeff Smathers <jsmathers@axionbio.com>
2024-02-05 19:02:44 -0500

    initial

---
+++ library/src/tic_tac_toe/logic/models.py
@@ -0,0 +1,11 @@
+# tick_tac_toe\logic\models.py
+
+import enum
+
+
+class Mark(enum.StrEnum):
+    CROSS = "X"
+    NAUGHT = "O"
+    @property
+    def other(self) -> "Mark":
+        return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
